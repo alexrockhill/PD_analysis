fcca11c3aecd7b30354f2e88fedd99fc6e3f1e8e
Alex Rockhill <aprockhill206@gmail.com>
2020-01-21 21:45:31 -0800

    syncing for portland

--- 03_preprocessing.py
+++ 03_preprocessing.py
@@ -1,6 +1,5 @@
 from swann.utils import (get_config, get_layout,
-                         exclude_subjects, get_behf,
-                         get_no_responses)
+                         exclude_subjects)
 from swann.preprocessing import find_ica, mark_autoreject
 from swann.viz import plot_find_bads, plot_ica
 
@@ -18,7 +17,7 @@ overwrite_eeg = \
           'they exist? (y/n)\n').upper() == 'Y'
 
 # loop across subjects
-'''for eegf in eegfs:
+for eegf in eegfs:
     plot_find_bads(eegf, overwrite=overwrite_eeg)
 
 # this will probably take ~5 minutes per subject, probably come back later
@@ -27,10 +26,8 @@ for eegf in eegfs:
 
 # need user input to select out blinks, sacades, heartbeak and muscle artifact
 for eegf in eegfs:
-    plot_ica(eegf, overwrite=overwrite_eeg)'''
+    plot_ica(eegf, overwrite=overwrite_eeg)
 
 # this will take even longer ~20+ minutes per subject depending on task length
 for eegf in eegfs:
-    behf = get_behf(eegf)
-    no_responses = get_no_responses(behf)
     mark_autoreject(eegf, overwrite=overwrite_eeg)
